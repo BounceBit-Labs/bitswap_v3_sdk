124790380500ffe069ded8786f4d84bd066e1fe3
dev <dev@bounce.finance>
2024-04-12 11:48:13 +0900

    feat: add universal router sdk

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./sdk-core.cjs.production.min.js')
+  module.exports = require('./universal-router-sdk.cjs.production.min.js')
 } else {
-  module.exports = require('./sdk-core.cjs.development.js')
+  module.exports = require('./universal-router-sdk.cjs.development.js')
 }
