76a70478210ca537e5645695a881e1b423a0d19a
dev <dev@bounce.finance>
2024-04-20 14:36:26 +0900

    feat: upgrade abi

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,13 +1,13 @@
 export declare const FACTORY_ADDRESS: {
-    1: string;
     6000: string;
     6001: string;
+    1: string;
 };
 export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export declare const POOL_INIT_CODE_HASH: {
-    1: string;
     6000: string;
     6001: string;
+    1: string;
 };
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
